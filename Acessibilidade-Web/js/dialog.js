c07304f76960125f86ac4995ba7419033caa7058
Paulo Barros <paulo.barros@lambda3.com.br>
2018-10-03 17:28:56 -0300

    Aula 06 - Dialog acessivel, tirando o foco e foco incoveniente

--- Acessibilidade-Web/js/dialog.js
+++ Acessibilidade-Web/js/dialog.js
@@ -3,16 +3,26 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
+
+btnAbreDialog.style.display = 'block';
 
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
+  video.removeAttribute('controls');
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
   dialog.classList.remove('dialogNewsletter--aberto');     
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
+  video.setAttribute('controls', true);
 }
 
 // Listeners
