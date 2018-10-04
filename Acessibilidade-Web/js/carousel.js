ff6af54e2496897955b1e566d938e570cd901f20
Paulo Barros <paulo.barros@lambda3.com.br>
2018-10-04 15:47:21 -0300

    Aula 07 - Deixando o carousel acessível

--- Acessibilidade-Web/js/carousel.js
+++ Acessibilidade-Web/js/carousel.js
@@ -1,9 +1,30 @@
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var noticias = document.querySelectorAll('.listaDeArtigos-item');
+
+// criando marcador de noticia selecionada
+var indicadorDeNoticiaSelecionada = document.createElement('span');
+indicadorDeNoticiaSelecionada.classList.add('escondeVisualmente');
+indicadorDeNoticiaSelecionada.textContent = 'Slide Atual';
+indicadorDeNoticiaSelecionada.id = 'indicadorDeNoticiaSelecionada';
+
+// exibir primeira notícia
+noticias[0].style.display= 'block';
 
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
+
+    // esconde todas as notícias
+    noticias.forEach(function(noticia){
+      noticia.style.display = 'none';
+    });
+
+    var index = btn.getAttribute('data-sliderItem');
+    noticias[index].style.display = 'block';
+
+    btn.appendChild(indicadorDeNoticiaSelecionada);
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
